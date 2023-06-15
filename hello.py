7748f1138a5722af2b721caf41515b0191dfbf2c
MUNGSE <132612617+MUNGSE@users.noreply.github.com>
2023-06-15 11:30:26 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+#comment
+name= input("enter name:")
+print("name is:"+name)
+marks1=int(input("enter marks1:"))
+marks2=int(input("enter marks2:"))
+marks3=int(input("enter marks3:"))
+print("marks of three sub:",marks1,marks2,marks3)
+avg=(marks1+marks2+marks3)/3
+print("avg is:",avg)
